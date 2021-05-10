e0c0d94ee5b601aac1bcdac7b137613491317e02
johann bzaih <johann.bzaih@gmail.com>
2021-05-10 14:30:14 +0200

    ✅ First rails done an tests passed

---
+++ test/system/questions_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+# class QuestionsTest < ApplicationSystemTestCase
+#   test "visiting the index" do
+#     visit questions_url
+
+#     assert_selector "h1", text: "Questions"
+#   end
+# end
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "label", text: "Ask your coach anything"
+  end
+end
